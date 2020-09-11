b8ff32f5f3de3ee9b67f62337439d97a039de679
이진하 <dkwlsfk2323@gmail.com>
2020-09-12 02:05:47 +0900

    kakao 불필요한 pods 삭제

---
+++ haruGomin/config/colorPalette.swift
@@ -0,0 +1,34 @@
+//
+//  colorPalette.swift
+//  haruGomin
+//
+//  Created by 이진하 on 2020/09/12.
+//  Copyright © 2020 이진하. All rights reserved.
+//
+
+import UIKit
+
+import UIKit
+
+struct ColorPalette {
+    private init() {}
+    // rgb 60 60 60
+    static let blackMain: UInt = 0x3c3c3c
+    // rgb 128 128 128
+    static let grayDarkMain: UInt = 0x808080
+    // rgb 152 152 152
+    static let graySoftMain: UInt = 0x989898
+    // rgb 254 221 55
+    static let yellowMain: UInt = 0xfedd37
+    // rgb 207 207 207
+    static let borderDark: UInt = 0xcfcfcf
+    // rgb 237 237 237
+    static let borderSoft: UInt = 0xededed
+    
+    static let background: UIColor = UIColor(red: 48/255, green: 53/255, blue: 67/255, alpha: 1)
+    static let hagoRed: UIColor = UIColor(red: 255/255, green: 73/255, blue: 59/255, alpha: 1)
+    static let darkBackground: UIColor =  UIColor(red: 33/255, green: 37/255, blue: 48/255, alpha: 1)
+    static let textGray:UIColor = UIColor(red: 203/255, green: 205/255, blue: 213/255, alpha: 1)
+
+}
+
---
+++ haruGomin/logIn/profile/setProfileViewController.swift
@@ -0,0 +1,17 @@
+//
+//  setProfileViewController.swift
+//  haruGomin
+//
+//  Created by 이진하 on 2020/09/12.
+//  Copyright © 2020 이진하. All rights reserved.
+//
+
+import UIKit
+
+class setProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}

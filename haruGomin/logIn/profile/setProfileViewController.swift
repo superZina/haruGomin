0a5b25c01123f5df6746026e6a6ff14881f566bc
이진하 <dkwlsfk2323@gmail.com>
2020-09-18 23:32:51 +0900

    이미지 추가

--- haruGomin/logIn/profile/setProfileViewController.swift
+++ haruGomin/logIn/profile/setProfileViewController.swift
@@ -10,12 +10,16 @@ import UIKit
 
 class setProfileViewController: UIViewController {
 
+    var ages:[String] = ["1 ~ 9" , "10 ~ 19", "20 ~ 29", "30 ~ 39", "40 ~ 49"]
+    var selectedAge:String = "1 ~ 9"
     @IBOutlet weak var profileImg: UIButton!
     @IBAction func selectImg(_ sender: Any) {
     }
     @IBOutlet weak var nickName: UITextField!
     @IBOutlet weak var age: UITextField!
     @IBOutlet weak var nextBtn: UIButton!
+    
+    var agePicker:UIPickerView! = UIPickerView()
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.isNavigationBarHidden = false
@@ -48,8 +52,72 @@ class setProfileViewController: UIViewController {
         self.nickName.addLeftPadding()
         self.age.addLeftPadding()
         
+        self.age.delegate = self
+        self.nickName.delegate = self
+        
+        agePicker.delegate = self
+        agePicker.dataSource = self
+        createAgePicker()
+        
         self.nextBtn.layer.cornerRadius = 8
         self.nextBtn.backgroundColor = ColorPalette.borderGray
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        if self.nickName.text == "" || self.age.text == "" {
+            self.nextBtn.isEnabled = false
+        }else{
+            self.nextBtn.isEnabled = true
+            self.nextBtn.backgroundColor = ColorPalette.hagoRed
+        }
+    }
+    
+    func createAgePicker() {
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        
+        let doneBtn = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(doneTimePressed))
+        let flexible = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        toolbar.setItems([flexible,doneBtn], animated: true)
+        age.inputAccessoryView = toolbar
+        agePicker.backgroundColor = ColorPalette.darkBackground
+        age.inputView = agePicker
 
+    }
+    @objc func doneTimePressed(){
+        self.age.text = selectedAge
+        self.view.endEditing(true)
+    }
+}
+
+extension setProfileViewController:UIPickerViewDelegate, UIPickerViewDataSource , UITextFieldDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return self.ages.count
+    }
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        var ageLabel: UILabel
+        ageLabel = UILabel()
+        ageLabel.font = UIFont.boldSystemFont(ofSize: 18)
+        ageLabel.textAlignment = .center
+        ageLabel.text = ages[row]
+        ageLabel.textColor = .white
+        
+        return ageLabel
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        selectedAge = ages[row]
+    }
+    func textFieldDidEndEditing(_ textField: UITextField, reason: UITextField.DidEndEditingReason) {
+        if self.nickName.text == "" || self.age.text == "" {
+            self.nextBtn.isEnabled = false
+            self.nextBtn.backgroundColor = ColorPalette.borderGray
+        }else{
+            self.nextBtn.isEnabled = true
+            self.nextBtn.backgroundColor = ColorPalette.hagoRed
+        }
+    }
 }

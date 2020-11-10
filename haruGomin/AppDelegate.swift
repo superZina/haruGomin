eb40fedeadd3708c45f94a746b39c377a0daa39d
이진하 <dkwlsfk2323@gmail.com>
2020-11-10 23:31:26 +0900

    로그인 화면 수정 , 자동로그인 구현

--- haruGomin/AppDelegate.swift
+++ haruGomin/AppDelegate.swift
@@ -13,10 +13,11 @@ import NaverThirdPartyLogin
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+    
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
         
         let instance = NaverThirdPartyLoginConnection.getSharedInstance()
         
@@ -35,40 +36,40 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         KakaoSDKCommon.initSDK(appKey: "7d5540f989b69a93d156335526309c23")
         return true
     }
-
+    
     // MARK: UISceneSession Lifecycle
-
+    
     func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
         NaverThirdPartyLoginConnection.getSharedInstance()?.application(app, open: url, options: options)
         return true
-      }
+    }
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
+    
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
+    
     // MARK: - Core Data stack
-
+    
     lazy var persistentContainer: NSPersistentContainer = {
         /*
          The persistent container for the application. This implementation
          creates and returns a container, having loaded the store for the
          application to it. This property is optional since there are legitimate
          error conditions that could cause the creation of the store to fail.
-        */
+         */
         let container = NSPersistentContainer(name: "haruGomin")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                 // Replace this implementation with code to handle the error appropriately.
                 // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                 
+                
                 /*
                  Typical reasons for an error here include:
                  * The parent directory does not exist, cannot be created, or disallows writing.
@@ -82,9 +83,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         })
         return container
     }()
-
+    
     // MARK: - Core Data Saving support
-
+    
     func saveContext () {
         let context = persistentContainer.viewContext
         if context.hasChanges {
@@ -98,6 +99,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             }
         }
     }
-
+    
 }
 
--- haruGomin/SceneDelegate.swift
+++ haruGomin/SceneDelegate.swift
@@ -9,6 +9,7 @@
 import UIKit
 import KakaoSDKAuth
 import NaverThirdPartyLogin
+import Alamofire
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
@@ -16,22 +17,137 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
         var startVC:UIViewController = UIViewController()
-        startVC = splashViewController()
-        //        guard let _ = (scene as? UIWindowScene) else { return }
-        if let windowScene = scene as? UIWindowScene {
-            let window = UIWindow(windowScene: windowScene)
-            window.rootViewController = UINavigationController(rootViewController: startVC)
-            window.rootViewController?.navigationController?.navigationBar.shadowImage = UIImage()
-            window.rootViewController?.navigationController?.navigationBar.isTranslucent = false
-            window.rootViewController?.navigationController?.navigationBar.barTintColor = .white
-            window.rootViewController?.navigationController?.navigationBar.backIndicatorTransitionMaskImage = UIImage(named: "arrowBackBlack")
-            window.rootViewController?.navigationController?.navigationBar.backIndicatorImage = UIImage(named: "arrowBackBlack")
-            window.rootViewController?.navigationController?.navigationBar.backItem?.title = ""
+        
+        let launchedBefore = UserDefaults.standard.bool(forKey: "launchedBefore")
+        if launchedBefore
+        {
+            print("Not first launch.")
+            //token이 존재한다면 -> 토큰검증
+            if let jwt = UserDefaults.standard.value(forKey: "jwt") {
+                print("token이 존재")
+                    let headers: HTTPHeaders = [
+                        HTTPHeader(name: "jwt", value: jwt as! String)
+                    ]
+                    let url = "http://52.78.127.67:8080/api/v1/users/check"
+                    AF.request(url , method: .post,headers:  headers)
+                        .validate()
+                        .responseJSON { (response) in
+                            switch response.result {
+                            case .success(let obj):
+                                
+                                do{
+                                    let dataJSON = try JSONSerialization.data(withJSONObject: obj, options: .prettyPrinted)
+                                    let getData = try JSONDecoder().decode(userInfo.self, from: dataJSON)
+                                    UserDefaults.standard.setValue(getData.userId, forKey: "userId")
+                                    UserDefaults.standard.setValue(getData.profileImage, forKey: "profileImage")
+                                    UserDefaults.standard.setValue(getData.ageRange, forKey: "ageRange")
+                                    UserDefaults.standard.setValue(getData.nickname, forKey: "userName")
+                                    print("DEBUG: userInfo is \(getData)")
+                                    
+                                    //로그인 성공 , 메인으로 넘어가기
+                                    
+                                    if getData.ageRange == 0 { //회원가입일때
+                                        UserDefaults.standard.setValue(true, forKey: "isLogin")
+                                        let profileVC = setProfileViewController()
+                                        if let windowScene = scene as? UIWindowScene {
+                                            let window = UIWindow(windowScene: windowScene)
+                                            window.rootViewController = UINavigationController(rootViewController: profileVC)
+                                            window.rootViewController?.navigationController?.navigationBar.shadowImage = UIImage()
+                                            window.rootViewController?.navigationController?.navigationBar.isTranslucent = false
+                                            window.rootViewController?.navigationController?.navigationBar.barTintColor = .white
+                                            window.rootViewController?.navigationController?.navigationBar.backIndicatorTransitionMaskImage = UIImage(named: "arrowBackBlack")
+                                            window.rootViewController?.navigationController?.navigationBar.backIndicatorImage = UIImage(named: "arrowBackBlack")
+                                            window.rootViewController?.navigationController?.navigationBar.backItem?.title = ""
+                                            
+                                            self.window = window
+                                            window.makeKeyAndVisible()
+                                        }
+                                    }else{ //이미 가입된 유저일때
+                                        
+                                        let mainVC = tabBarViewController()
+                                        if let windowScene = scene as? UIWindowScene {
+                                            let window = UIWindow(windowScene: windowScene)
+                                            window.rootViewController = UINavigationController(rootViewController: mainVC)
+                                            window.rootViewController?.navigationController?.navigationBar.shadowImage = UIImage()
+                                            window.rootViewController?.navigationController?.navigationBar.isTranslucent = false
+                                            window.rootViewController?.navigationController?.navigationBar.barTintColor = .white
+                                            window.rootViewController?.navigationController?.navigationBar.backIndicatorTransitionMaskImage = UIImage(named: "arrowBackBlack")
+                                            window.rootViewController?.navigationController?.navigationBar.backIndicatorImage = UIImage(named: "arrowBackBlack")
+                                            window.rootViewController?.navigationController?.navigationBar.backItem?.title = ""
+                                            
+                                            self.window = window
+                                            window.makeKeyAndVisible()
+                                        }
+                                        
+                                    }
+                                    
+                                }catch{
+                                    print(error.localizedDescription)
+                                }
+                            case .failure(let error):
+                                
+                                print(error.localizedDescription)
+                                
+                                startVC = logInViewController()
+                                if let windowScene = scene as? UIWindowScene {
+                                    let window = UIWindow(windowScene: windowScene)
+                                    window.rootViewController = UINavigationController(rootViewController: startVC)
+                                    window.rootViewController?.navigationController?.navigationBar.shadowImage = UIImage()
+                                    window.rootViewController?.navigationController?.navigationBar.isTranslucent = false
+                                    window.rootViewController?.navigationController?.navigationBar.barTintColor = .white
+                                    window.rootViewController?.navigationController?.navigationBar.backIndicatorTransitionMaskImage = UIImage(named: "arrowBackBlack")
+                                    window.rootViewController?.navigationController?.navigationBar.backIndicatorImage = UIImage(named: "arrowBackBlack")
+                                    window.rootViewController?.navigationController?.navigationBar.backItem?.title = ""
+                                    
+                                    self.window = window
+                                    window.makeKeyAndVisible()
+                                }
+                            default:
+                                return
+                            }
+                        }
+            }else{
+                //토큰이 유효하지 않는다면
+                print("token is not available")
+                startVC = logInViewController()
+                if let windowScene = scene as? UIWindowScene {
+                    let window = UIWindow(windowScene: windowScene)
+                    window.rootViewController = UINavigationController(rootViewController: startVC)
+                    window.rootViewController?.navigationController?.navigationBar.shadowImage = UIImage()
+                    window.rootViewController?.navigationController?.navigationBar.isTranslucent = false
+                    window.rootViewController?.navigationController?.navigationBar.barTintColor = .white
+                    window.rootViewController?.navigationController?.navigationBar.backIndicatorTransitionMaskImage = UIImage(named: "arrowBackBlack")
+                    window.rootViewController?.navigationController?.navigationBar.backIndicatorImage = UIImage(named: "arrowBackBlack")
+                    window.rootViewController?.navigationController?.navigationBar.backItem?.title = ""
+                    
+                    self.window = window
+                    window.makeKeyAndVisible()
+                }
+            }
             
-            self.window = window
-            window.makeKeyAndVisible()
         }
+        else{
+            //처음 실행하는거라면(로그인도 안되어있음)
+            print("First launch")
+            startVC = splashViewController()
+            UserDefaults.standard.set(true, forKey: "launchedBefore")
+            if let windowScene = scene as? UIWindowScene {
+                let window = UIWindow(windowScene: windowScene)
+                window.rootViewController = UINavigationController(rootViewController: startVC)
+                window.rootViewController?.navigationController?.navigationBar.shadowImage = UIImage()
+                window.rootViewController?.navigationController?.navigationBar.isTranslucent = false
+                window.rootViewController?.navigationController?.navigationBar.barTintColor = .white
+                window.rootViewController?.navigationController?.navigationBar.backIndicatorTransitionMaskImage = UIImage(named: "arrowBackBlack")
+                window.rootViewController?.navigationController?.navigationBar.backIndicatorImage = UIImage(named: "arrowBackBlack")
+                window.rootViewController?.navigationController?.navigationBar.backItem?.title = ""
+                
+                self.window = window
+                window.makeKeyAndVisible()
+            }
+        }
+        
     }
     
     func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
